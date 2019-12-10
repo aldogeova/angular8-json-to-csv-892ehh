bfad48984ffcecaa0cc58ccd86dad8285d3d81a7
aldogeova <aldogeova@gmail.com>
2019-12-10 16:17:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular8-json-to-csv-892ehh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular8-json-to-csv-892ehh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { AppService } from './app.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+  constructor(private appService:AppService) {  }
+
+    jsonData =  [
+      {
+        name: "Anil Singh",
+        age: 33,
+        average: 98,
+        approved: true,
+        description: "I am active blogger and Author."
+      },
+      {
+        name: 'Reena Singh',
+        age: 28,
+        average: 99,
+        approved: true,
+        description: "I am active HR."
+      },
+      {
+        name: 'Aradhya',
+        age: 4,
+        average: 99,
+        approved: true,
+        description: "I am engle."
+      },
+    ];
+
+  download(){
+    this.appService.downloadFile(this.jsonData, 'jsontocsv');
+  }
+}
